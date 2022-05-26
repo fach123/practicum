362ab17a1f5d4ab95e89ff7cba3a0ca76b648534
unknown <fach322@gmail.com>
2022-05-27 00:30:30 +0700

    release

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import AppHeader from './components/AppHeader/AppHeader';
-import style from './index.module.css';
+import React, {useEffect, useState} from 'react';
+import AppHeader from './components/app-header/app-header'
+import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
+import BurgerConstructor from "./components/burger-constructor/burger-constructor";
+import style from './App.module.css';
+import './index.css';
+
+const apiUrl = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
-  return (
-      <div className={style.main}>
-    <AppHeader></AppHeader>
-      </div>
-  );
+    const [apiData, setApiData] = useState(null);
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => setApiData(data))
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, [])
+
+    return (
+        <>
+            <AppHeader/>
+            <div className={style.inner}>
+                {apiData && <BurgerIngredients apiData={apiData} />}
+                {apiData && <BurgerConstructor apiData={apiData}/>}
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,32 @@
+import style from "./app-header.module.css";
+import {ProfileIcon, BurgerIcon, Logo, ListIcon,} from "@ya.praktikum/react-developer-burger-ui-components";
+
+const AppHeader = () => {
+    return (
+        <header className={style.header}>
+            <nav className={`${style.menu} pt-4 pb-4`}>
+                <div className={style.inner}>
+                    <a href="/" className={`${style.link_active} pr-5 pl-5 pt-4 pb-4`}>
+                        <BurgerIcon type="primary"/>
+                        <p className="text text_type_main-default ml-2">Конструктор</p>
+                    </a>
+
+                    <a href="/" className={`${style.link} pr-5 pl-5 pt-4 pb-4 ml-2`}>
+                        <ListIcon type="secondary"/>
+                        <p className="text text_type_main-default ml-2">Лента заказов</p>
+                    </a>
+
+                    <div className={style.logotype}>
+                        <Logo/>
+                    </div>
+                    <a href="/" className={`${style.link_personal} pr-5 pl-5 pt-4 pb-4`}>
+                        <ProfileIcon type="secondary"/>
+                        <p className="text text_type_main-default ml-2">Личный кабинет</p>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default AppHeader;
