9d68dad72f3db45f3c4aafc231753303959311a1
fach123 <81368990+fach123@users.noreply.github.com>
2022-05-27 02:13:31 +0700

    Revert "Sprint1/step2"

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import AppHeader from './components/app-header/app-header'
-import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
-import BurgerConstructor from "./components/burger-constructor/burger-constructor";
-import style from './App.module.css';
-import './index.css';
-
-const apiUrl = 'https://norma.nomoreparties.space/api/ingredients';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [apiData, setApiData] = useState(null);
-
-    useEffect(() => {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => setApiData(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, [])
-
-    return (
-        <>
-            <AppHeader/>
-            <div className={style.inner}>
-                {apiData && <BurgerIngredients apiData={apiData} />}
-                {apiData && <BurgerConstructor apiData={apiData}/>}
-            </div>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
